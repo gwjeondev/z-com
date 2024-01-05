39c1e4c38efdf030dcfd313b44ca2163c6500073
gwjeon <gw4609@gmail.com>
2024-01-05 21:36:35 +0900

    섹션1 - 클라이언트 컴포넌트로 전환하기

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import styles from '@/app/page.module.css'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
