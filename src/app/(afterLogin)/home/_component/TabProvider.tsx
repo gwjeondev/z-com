5c0d4e8b499bd6c4feeb968fc7792b3ddcfbe501
gwjeon <gw4609@gmail.com>
2024-01-09 21:37:07 +0900

    섹션2 - PostForm 만들기(타이핑 외우기)

---
+++ src/app/(afterLogin)/home/_component/TabProvider.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import { createContext, useState } from 'react'
+
+export const TabContext = createContext({
+  tab: 'rec',
+  setTab: (value: 'rec' | 'fol'): void => {},
+})
+
+export default function TabProvider({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const [tab, setTab] = useState('rec')
+
+  return (
+    <TabContext.Provider value={{ tab, setTab }}>
+      {children}
+    </TabContext.Provider>
+  )
+}
+//
--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,3 +1,29 @@
+import style from './home.module.css'
+import Post from '@/app/(afterLogin)/_component/Post'
+import PostForm from './_component/PostForm'
+import TabProvider from './_component/TabProvider'
+import Tab from './_component/Tab'
+
 export default function () {
-  return <div>home</div>
+  return (
+    <main className={style.main}>
+      <TabProvider>
+        <Tab />
+        <PostForm />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </TabProvider>
+    </main>
+  )
 }
